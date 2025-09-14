1542662e92b93a58c5ebcec922241abb231e54a5
Ana Stancic <anet.persa@gmail.com>
2025-09-14 20:48:54 +0200

    App created, application.properties updated

---
+++ src/main/java/rs/ac/singidunum/programski_jezici_2023204418_IMIReserve/ProgramskiJezici2023204418ImiReserveApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.programski_jezici_2023204418_IMIReserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramskiJezici2023204418ImiReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramskiJezici2023204418ImiReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/singidunum/programski_jezici_2023204418_IMIReserve/ProgramskiJezici2023204418ImiReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.programski_jezici_2023204418_IMIReserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramskiJezici2023204418ImiReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
