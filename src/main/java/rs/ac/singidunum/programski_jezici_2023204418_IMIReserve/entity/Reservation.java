06ffd1507a11b5094d9dba5d727a039c7ac6433f
Ana Stancic <anet.persa@gmail.com>
2025-09-15 18:29:31 +0200

    Reservation Entity and Repo created

---
+++ src/main/java/rs/ac/singidunum/programski_jezici_2023204418_IMIReserve/entity/Reservation.java
@@ -0,0 +1,56 @@
+package rs.ac.singidunum.programski_jezici_2023204418_IMIReserve.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reservation")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
+    private Integer id;
+
+    @Column(name = "start_time", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime endTime;
+
+    // dodatni parametar eksperimenta (npr. temperatura, broj ciklusa itd.)
+    @Column(name = "parameter")
+    private String parameter;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_researcher", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Researcher researcher;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_instrument", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Instrument instrument;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @JsonIgnore
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
---
+++ src/main/java/rs/ac/singidunum/programski_jezici_2023204418_IMIReserve/repository/ReservationRepository.java
@@ -0,0 +1,23 @@
+package rs.ac.singidunum.programski_jezici_2023204418_IMIReserve.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rs.ac.singidunum.programski_jezici_2023204418_IMIReserve.entity.Instrument;
+import rs.ac.singidunum.programski_jezici_2023204418_IMIReserve.entity.Researcher;
+import rs.ac.singidunum.programski_jezici_2023204418_IMIReserve.entity.Reservation;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+    List<Reservation> findAllByDeletedAtIsNull();
+
+    Optional<Reservation> findByIdAndDeletedAtIsNull(Integer id);
+
+    List<Reservation> findAllByResearcher_IdAndDeletedAtIsNull(Integer researcherId);
+
+    List<Reservation> findAllByInstrument_IdAndDeletedAtIsNull(Integer instrumentId);
+
+    Boolean existsByIdAndDeletedAtIsNull(Integer id);
+}
